da5c81d4499233f6cde054e3badbacaf48b655b3
Kai Gellien <kai.gellien@web.de>
2017-04-02 22:01:57 +0200

    back analog zu forward implementiert; renames als Vorbereitung auf commands

--- src/main/scala/de/gellien/marsrover/Rover.scala
+++ src/main/scala/de/gellien/marsrover/Rover.scala
@@ -1,22 +1,28 @@
 package de.gellien.marsrover
 
 class Rover(var position: Point, var direction: Direction) {
-  def l = direction = direction match {
+  def turnLeft = direction = direction match {
     case N => W
     case E => N
     case S => E
     case W => S
   }
-  def r = direction = direction match {
+  def turnRight = direction = direction match {
     case N => E
     case E => S
     case S => W
     case W => N
   }
-  def f = position = direction match {
+  def forward = position = direction match {
     case N => Mars.north(position)
     case E => Mars.east(position)
     case S => Mars.south(position)
     case W => Mars.west(position)
+  }
+  def back = position = direction match {
+    case N => Mars.south(position)
+    case E => Mars.west(position)
+    case S => Mars.north(position)
+    case W => Mars.east(position)
   }
 }
--- src/test/scala/de/gellien/marsrover/RoverSpec.scala
+++ src/test/scala/de/gellien/marsrover/RoverSpec.scala
@@ -2,27 +2,32 @@ package de.gellien.marsrover
 import org.specs2.mutable._
 
 class RoverSpec extends SpecificationWithJUnit {
+  val start = Point(10, 10)
   "A Rover must be able to turn:" >> {
-    val start = Point(10, 10)
     "turnLeft moves from N to W" >> {
       val rover = new Rover(start, N)
       val expectedDirection = W
-      rover.l
+      rover.turnLeft
       rover.direction must_== expectedDirection
     }
     "turnRight moves from N to E" >> {
       val rover = new Rover(start, N)
       val expectedDirection = E
-      rover.r
+      rover.turnRight
       rover.direction must_== expectedDirection
     }
   }
   "A Rover must be able to move one step at a time:" >> {
-    val start = Point(10, 10)
     "forward moves one step into rover.direction" >> {
       val rover = new Rover(start, N)
       val expectedPosition = Point(10, 11)
-      rover.f
+      rover.forward
+      rover.position must_== expectedPosition
+    }
+    "back moves one step opposite to rover.direction" >> {
+      val rover = new Rover(start, S)
+      val expectedPosition = Point(10, 11)
+      rover.back
       rover.position must_== expectedPosition
     }
   }
