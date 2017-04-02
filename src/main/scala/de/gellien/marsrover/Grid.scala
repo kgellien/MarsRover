9e31fca81edd0ccc64b8823f99969c244f23cd26
Kai Gellien <kai.gellien@web.de>
2017-04-02 13:43:15 +0200

    Grid.south implementiert

--- src/main/scala/de/gellien/marsrover/Grid.scala
+++ src/main/scala/de/gellien/marsrover/Grid.scala
@@ -4,7 +4,11 @@ case class Point(x: Int, y: Int)
 
 class Grid(val maxX: Int, val maxY: Int) {
   def north(p: Point): Point = p.copy(y = (p.y + 1) % maxY)
-  def south(p: Point): Point = ???
+  def south(p: Point): Point = {
+    var newY = p.y - 1
+    while (newY < 0) newY = newY + maxY
+    p.copy(y = newY % maxY) // % maxY bleibt, damit es auch für übergroßes y funktioniert
+  }
   def east(p: Point): Point = ???
   def west(p: Point): Point = ???
 }
--- src/test/scala/de/gellien/marsrover/GridSpec.scala
+++ src/test/scala/de/gellien/marsrover/GridSpec.scala
@@ -11,10 +11,20 @@ class GridSpec extends SpecificationWithJUnit {
       val expectedPosition = Point(10, 11)
       grid.north(start) must_== expectedPosition
     }
-    "movement to the north wraps when start has y == maxY-1 the y-coordinate" >> {
+    "movement to the north wraps the y-coordinate when start has y == maxY-1" >> {
       val start = Point(10, maxY - 1)
       val expectedPosition = Point(10, 0)
       grid.north(start) must_== expectedPosition
+    }
+    "movement to the south should decrement the y-coordinate only" >> {
+      val start = Point(10, 10)
+      val expectedPosition = Point(10, 9)
+      grid.south(start) must_== expectedPosition
+    }
+    "movement to the south wraps the y-coordinate when start has y == 0" >> {
+      val start = Point(10, 0)
+      val expectedPosition = Point(10, maxY - 1)
+      grid.south(start) must_== expectedPosition
     }
   }
 }
